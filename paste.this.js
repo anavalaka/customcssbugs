2300cdc84dc66adbcb2f8bbabd86a5307ef96c42
anavalaka <arnold.naval@akanewmedia.com>
2021-11-16 16:30:19 -0500

    pastethis

--- paste.this.js
+++ paste.this.js
@@ -3,6 +3,7 @@ const id = 'my-injected-style';
 setInterval(() => {
   document.getElementById(id)?.remove();
   const link = document.createElement('link');
+  link.id = id;
   link.rel = 'stylesheet';
   link.href = `https://any.thing/output/style.css?time=${new Date().getTime()}`;
   document.head.prepend(link);
