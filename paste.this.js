aa364ea86c6deb0cf9e858b97e55cdf1cc24228d
anavalaka <arnold.naval@akanewmedia.com>
2021-11-17 09:39:25 -0500

    gitignore stuff

--- paste.this.js
+++ paste.this.js
@@ -2,6 +2,9 @@
 // chrome://flags/#allow-insecure-localhost
 // then run this command "yarn run sass <filename.scss> output/style.css --watch"
 
+// as part of initial setup, you need to cd into localhost-certs/ folder
+// then run this command openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -keyout key.pem -out cert.pem
+
 const id = 'my-injected-style';
 document.getElementById(id)?.remove();
 const link = document.createElement('link');
