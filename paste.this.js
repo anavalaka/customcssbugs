5d46f7ee853754378a9b06ec14337bf6c72dc6b2
anavalaka <arnold.naval@akanewmedia.com>
2021-11-17 08:45:49 -0500

    meow

--- paste.this.js
+++ paste.this.js
@@ -3,12 +3,11 @@
 // then run this command "yarn run sass <filename.scss> output/style.css --watch"
 
 const id = 'my-injected-style';
-
+document.getElementById(id)?.remove();
+const link = document.createElement('link');
+link.id = id;
+link.rel = 'stylesheet';
+document.head.prepend(link);
 setInterval(() => {
-  document.getElementById(id)?.remove();
-  const link = document.createElement('link');
-  link.id = id;
-  link.rel = 'stylesheet';
   link.href = `https://127.0.0.1:64666/output/style.css?time=${new Date().getTime()}`;
-  document.head.prepend(link);
 }, 3000);
